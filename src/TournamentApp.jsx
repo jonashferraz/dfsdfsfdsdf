195054f0def7978a4f0313c97166bd36a19301f6
jonashferraz <upgradeinformaticanvt@gmail.com>
2025-05-19 14:26:44 -0300

    Add files via upload

---
+++ src/TournamentApp.jsx
@@ -0,0 +1,2 @@
+import React, { useEffect, useState } from "react";
+// ... (código omitido para brevidade neste bloco)
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TournamentApp from './TournamentApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TournamentApp />
+  </React.StrictMode>
+);
